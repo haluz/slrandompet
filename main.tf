cadcd3966831b50d0bb17c5dc0c49cb92503a8c0
Ievgen Khaliuzov <ievgenk@spacelift.io>
2026-04-02 21:11:45 +0100

    some change

--- main.tf
+++ main.tf
@@ -1,3 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+data "aws_caller_identity" "current" {}
+
+output "aws_account_id" {
+  value = data.aws_caller_identity.current.account_id
+}
+
 resource "random_pet" "orbit_mascot" {
   length = 2
 }
