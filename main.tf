105de76dd700a32f5f473837d9987b497914efa8
Ievgen Khaliuzov <ievgenk@spacelift.io>
2026-04-03 14:50:42 +0100

    add vpc change

--- main.tf
+++ main.tf
@@ -32,3 +32,27 @@ resource "aws_s3_bucket" "orbit_storage" {
     environment = "demo"
   }
 }
+
+variable "subnet_id" {
+  type        = string
+  description = "ID of the subnet from networking stack"
+}
+
+data "aws_subnet" "selected" {
+  id = var.subnet_id
+}
+
+data "aws_vpc" "selected" {
+  id = data.aws_subnet.selected.vpc_id
+}
+
+resource "aws_security_group" "app" {
+  name        = "orbit-labs-app-sg"
+  description = "Security group for Orbit Labs app"
+  vpc_id      = data.aws_vpc.selected.id
+
+  tags = {
+    name    = "Orbit Labs App SG"
+    project = "Orbit-labs"
+  }
+}
